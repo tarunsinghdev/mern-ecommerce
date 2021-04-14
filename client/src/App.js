f84b802e5884db29c8091c50a009c104dad7bd8d
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-04-14 12:13:02 +0530

    Setting up the frontend

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
