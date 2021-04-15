6251513c554e862457d979b923ce0a091d544199
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-04-15 12:49:49 +0530

    Changed to ES modules import syntax

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,9 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center" py-3>
-            Copyright &copy; eMart
-          </Col>
+          <Col className="text-center py-3">Copyright &copy; eMart</Col>
         </Row>
       </Container>
     </footer>
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
 
-const products = require('./data/products');
+import products from './data/products.js';
 
 dotenv.config();
 
