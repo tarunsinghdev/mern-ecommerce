d4cb9006d4e648407423de478d88028d98d078df
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-04-26 16:40:07 +0530

    Setup the models

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
