f5d2d4b31b192b2f9527e1e66f567404f857c1a8
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-04-28 12:34:50 +0530

    Code cleanup with controllers

---
+++ server/controllers/productController.js
@@ -0,0 +1,19 @@
+import asyncHandler from 'express-async-handler';
+import Product from '../models/product.js';
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProducts, getProductById };
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,29 +1,13 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-
-import Product from '../models/product.js';
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.get('/', getProducts);
 
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.get('/:id', getProductById);
 
 export default router;
