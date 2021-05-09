254e4d950abd11f6accc6e2727739cd19f244c5c
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-05-09 12:11:52 +0530

    Create product - backend(Admin only)

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -27,4 +27,57 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/image/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    countInStock,
+    category,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,17 +1,21 @@
 import express from 'express';
 
 import {
+  createProduct,
   deleteProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.post('/', protect, admin, createProduct);
 
 router.get('/:id', getProductById);
 router.delete('/:id', protect, admin, deleteProduct);
+router.put('/:id', protect, admin, updateProduct);
 
 export default router;
