2ec0654b1cab61a918dca528c51c72ad35bce6ab
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-05-10 22:12:35 +0530

    Setup 'edit-product' page(Admin only)

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import OrderDetailsScreen from './screens/OrderDetailsScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
+          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/productlist" component={ProductListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
